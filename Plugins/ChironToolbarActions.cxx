1d8b5285ed9d28a40d2ba35c0998076b34dd09b2
Jérôme Velut <jerome.velut@gmail.com>
2009-11-16 22:26:49 +0100

    Base tree generation. Chiron has a minimal environment + expose vtkDICOMImageReader to paraview
    Under Plugins: * ChironToolbarActions is the entry point of the plugin
                   * CMakeLists defines the plugin project
    Under Resources: * some XML used by paraview for the plugin definition
                     * Qt icons (png) and resources (.qrc)

---
+++ Plugins/ChironToolbarActions.cxx
@@ -0,0 +1,128 @@
+#include "ChironToolbarActions.h"
+
+#include <QApplication>
+#include <QStyle>
+#include <QWheelEvent>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QSlider>
+#include <QObject>
+#include <iostream>
+#include "QVTKWidget.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkRenderWindow.h"
+
+#include "pqApplicationCore.h"
+#include "pqMainWindowCore.h"
+#include "pqObjectBuilder.h"
+#include "pqServer.h"
+#include "pqView.h"
+#include "pqRenderView.h"
+
+#include "pqActiveView.h"
+#include "pqPipelineSource.h"
+#include "pqDisplayPolicy.h"
+#include "pqOutputPort.h"
+#include "pqServerManagerModel.h"
+#include "pqServerManagerSelectionModel.h"
+#include "pqUndoStack.h"
+#include "vtkSMProxy.h"
+#include "vtkSMViewProxy.h"
+#include "vtkSMOutputPort.h"
+#include "vtkSMRenderViewProxy.h"
+#include "vtkSMDimensionsDomain.h"
+#include "vtkCamera.h"
+#include "vtkSMDoubleVectorProperty.h"
+#include "vtkSMIntVectorProperty.h"
+#include "pqViewManager.h"
+#include "pqPropertyManager.h"
+#include "pqMultiViewFrame.h"
+#include "pqDataRepresentation.h"
+#include "pqServerResource.h"
+
+#include "vtkEventQtSlotConnect.h"
+//-----------------------------------------------------------------------------
+ChironToolbarActions::ChironToolbarActions(QObject* p) : QActionGroup(p)
+{
+   // First Chiron toolbar button is checkable, but not exclusive
+   this->setExclusive( false );
+
+   // Adding buttons (ie actions) to the plugin's toolbar
+   QAction* a;
+   // Define a new action : Icon, tool tip and parent
+   a = new QAction( QIcon(":/ToolbarIcons/chiron-256x256.png"), 
+                    "Enable Chiron", 
+                    this );
+   a->setCheckable( true );
+   
+   // setData give the possibility to determine which button has been pressed
+   a->setData("EnableChiron");
+
+   // Add this action to the action group, ie the toolbar
+   this->addAction(a);
+
+   // New actions will be added here (property panels,...)
+   // BEGIN NEW ACTIONS
+ 
+   // END NEW ACTIONS   
+
+   // Connect the signal triggered by the toolbar 
+   // to the slot (ie function) onAction. 'triggered' 
+   // is a signal sent when one of the toolbar button is pressed
+   QObject::connect(this, SIGNAL(triggered(QAction*)), 
+                    this, SLOT(onAction(QAction*)));
+}
+
+
+//-----------------------------------------------------------------------------
+void ChironToolbarActions::onAction(QAction* a)
+{
+   // Depending on which toolbar button has been clicked,
+   // different actions are performed
+   pqApplicationCore* core = pqApplicationCore::instance();
+   pqObjectBuilder* builder = core->getObjectBuilder();
+   pqServerManagerModel* sm = core->getServerManagerModel();
+   pqServerManagerSelectionModel* smSel = core->getSelectionModel( );
+
+   // item represents the current selected proxy in the pipeline browser
+   pqServerManagerModelItem* item = smSel->currentItem();
+
+   QString actionStr = a->data().toString();
+
+   if( actionStr == QString( "EnableChiron") )
+   {
+      // When Chiron is enabled, view creations are detected and connected
+      // to each module
+      this->connectToViewCreation( a->isChecked( ));
+   }
+}
+
+void ChironToolbarActions::connectToViewCreation( bool connect )
+{
+   pqApplicationCore* core = pqApplicationCore::instance();
+   pqObjectBuilder* builder = core->getObjectBuilder();
+
+   if( connect )
+      QObject::connect(builder, 
+                       SIGNAL(viewCreated(pqView*)),
+                       this,
+                       SLOT(connectToChironModules(pqView*)));
+   else
+      QObject::disconnect(builder, 
+                          SIGNAL(viewCreated(pqView*)), 
+                          this,
+                          SLOT(connectToChironModules(pqView*)));
+}
+
+
+void ChironToolbarActions::connectToChironModules( pqView* view )
+{
+  QVTKWidget* qvtkRenderWidget = qobject_cast<QVTKWidget*>(view->getWidget());
+
+  vtkRenderWindow* renWin = qvtkRenderWidget->GetRenderWindow();
+  vtkRenderWindowInteractor* iren = renWin->GetInteractor(  );
+  cout << "connection pqView " << view << " to Chiron modules"<<endl;
+}
+
+
+
---
+++ Plugins/ChironToolbarActions.h
@@ -0,0 +1,44 @@
+//! \class ChironToolbarActions
+//! \brief Main entry for the Chiron ParaView plugin
+//!
+//! As soon as the Chiron plugin loads, this toolbar is created in ParaView.
+//! Chiron toolbar has -for now- 1 button:
+//!    - Enable Chiron
+//!      When future views are created, Chiron modules are connected to
+//!      them and play their role (event catching, overlay conf., ...)
+//!
+//! \todo Implements Chiron abstract module
+//! \bug In release mode, QVTKWidget crashes ParaView when manipulating its
+//! interactor. \see http://public.kitware.com/Bug/view.php?id=9895
+//!
+//! \author Jerome Velut
+//! \date 07 november 2009
+
+#ifndef __CHIRONTOOLBARACTIONS_H__
+#define __CHIRONTOOLBARACTIONS_H__
+
+// QT includes
+#include <QActionGroup>
+
+// ParaView includes
+#include <pqView.h>
+
+class ChironToolbarActions : public QActionGroup
+{
+  Q_OBJECT
+public:
+  //! Constructor: Defines Qt Slot/Signal connection
+  ChironToolbarActions(QObject* p);
+
+public slots:
+  //! Callback for each action triggerred.
+  //! This function is called after a click on a button of the toolBar
+  void onAction(QAction* a);
+  void connectToChironModules( pqView* );
+
+private:
+   void connectToViewCreation( bool on );
+};
+
+#endif //__CHIRONTOOLBARACTIONS_H__
+
