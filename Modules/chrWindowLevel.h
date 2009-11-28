0a999453928ec9cca165aae4701b4802154fa726
Jérôme Velut <jerome.velut@gmail.com>
2009-11-28 22:46:16 +0100

    [DOC] Licensing all files

--- Modules/chrWindowLevel.h
+++ Modules/chrWindowLevel.h
@@ -1,3 +1,21 @@
+//    Copyright Jerome Velut 2009
+//
+//    This file is part of Chiron.
+//
+//    Chiron is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Chiron is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You have received a copy of the GNU General Public License
+//    along with Chiron in the file COPYING.  
+//    If not, see <http://www.gnu.org/licenses/>.
+ 
 #ifndef __CHRWINDOWLEVEL_H__
 #define __CHRWINDOWLEVEL_H__
 
