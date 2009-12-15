4f341ccaab668e0a7e01a5a2e52ac960cd164789
jeromevelut <jerome.velut@gmail.com>
2009-12-15 23:14:53 +0100

    [ENH] First VTK algorithm: Polyline source. A new plugin is created from these algo

---
+++ Algorithms/vtkPolylineSource.cxx
@@ -0,0 +1,92 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkPolylineSource.cxx,v $
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkPolylineSource.h"
+
+#include "vtkCellArray.h"
+#include "vtkFloatArray.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkObjectFactory.h"
+#include "vtkStreamingDemandDrivenPipeline.h"
+#include "vtkPointData.h"
+#include "vtkPoints.h"
+#include "vtkPolyData.h"
+
+#include <math.h>
+vtkCxxRevisionMacro(vtkPolylineSource, "$Revision: 1.48 $");
+vtkStandardNewMacro(vtkPolylineSource);
+
+vtkPolylineSource::vtkPolylineSource()
+{
+  this->Points = 0;
+  this->SetNumberOfInputPorts(0);
+}
+
+int vtkPolylineSource::RequestInformation(
+  vtkInformation *vtkNotUsed(request),
+  vtkInformationVector **vtkNotUsed(inputVector),
+  vtkInformationVector *outputVector)
+{
+  // get the info object
+  vtkInformation *outInfo = outputVector->GetInformationObject(0);
+  outInfo->Set(vtkStreamingDemandDrivenPipeline::MAXIMUM_NUMBER_OF_PIECES(),
+               -1);
+  return 1;
+}
+
+int vtkPolylineSource::RequestData(
+  vtkInformation *vtkNotUsed(request),
+  vtkInformationVector **vtkNotUsed(inputVector),
+  vtkInformationVector *outputVector)
+{
+  // get the info object
+  vtkInformation *outInfo = outputVector->GetInformationObject(0);
+
+  // get the ouptut
+  vtkPolyData *output = vtkPolyData::SafeDownCast(
+    outInfo->Get(vtkDataObject::DATA_OBJECT()));
+
+  vtkPoints *newPoints; 
+  vtkCellArray *newLines;
+  
+  if (outInfo->Get(vtkStreamingDemandDrivenPipeline::UPDATE_PIECE_NUMBER()) > 0)
+    {
+    return 1;
+    }
+
+  newPoints = vtkPoints::New();
+  newLines = vtkCellArray::New();
+  newLines->InsertNextCell( this->Points?this->Points->GetNumberOfPoints():0 );
+
+  if( this->Points )
+     for( int id = 0; id < this->Points->GetNumberOfPoints( ); id ++ )//
+     {
+        vtkIdType newId = newPoints->InsertNextPoint(this->Points->GetPoint( id ));
+        newLines->InsertCellPoint( newId );
+    }
+  
+    output->SetPoints(newPoints);
+  newPoints->Delete();
+
+  output->SetLines(newLines);
+  newLines->Delete();
+
+  return 1;
+}
+
+void vtkPolylineSource::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
---
+++ Algorithms/vtkPolylineSource.h
@@ -0,0 +1,58 @@
+//    Copyright Jerome Velut 2009
+//
+//    This file is part of Chiron.
+//
+//    Chiron is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Chiron is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You have received a copy of the GNU General Public License
+//    along with Chiron in the file COPYING.  
+//    If not, see <http://www.gnu.org/licenses/>.
+ 
+//! \class vtkPolylineSource
+//! \brief VTK source based on a set of vtkPoints
+//!
+//! \todo Comments
+//! 
+//! \author Jerome Velut
+//! \date 14 dec 2009
+
+#ifndef __VTKPOLYLINESOURCE_H__
+#define __VTKPOLYLINESOURCE_H__
+
+#include "vtkPolyDataAlgorithm.h"
+#include "vtkPoints.h"
+
+class VTK_EXPORT vtkPolylineSource : public vtkPolyDataAlgorithm 
+{
+public:
+  static vtkPolylineSource *New();
+  vtkTypeRevisionMacro(vtkPolylineSource,vtkPolyDataAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Set position of first end point.
+  vtkSetObjectMacro(Points,vtkPoints);
+  vtkGetObjectMacro(Points,vtkPoints);
+
+protected:
+  vtkPolylineSource( );
+  ~vtkPolylineSource() {};
+
+  int RequestData(vtkInformation *, vtkInformationVector **, vtkInformationVector *);
+  int RequestInformation(vtkInformation *, vtkInformationVector **, vtkInformationVector *);
+  vtkPoints* Points;
+
+private:
+  vtkPolylineSource(const vtkPolylineSource&);  // Not implemented.
+  void operator=(const vtkPolylineSource&);  // Not implemented.
+};
+
+#endif //__VTKPOLYLINESOURCE_H__
